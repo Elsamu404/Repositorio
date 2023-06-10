d1ec660ec69f354d51d4efa4ba378fe29b15f594
Eliel Samuel Garcia Martinez <127472305+Elsamu404@users.noreply.github.com>
2023-06-09 23:04:59 -0600

    Add files via upload

---
+++ Objetos.js
@@ -0,0 +1,34 @@
+//Creamos un objeto
+var myObject = {
+   nombre : "juan",
+    edad : 50,
+     genero : "masculino",
+      estatura : 1.65,
+       nacionalidad : "mexicana",
+        estado : "oaxaca"
+
+};
+// accedemos a las propiedades usando notacion corchetes y ademas sin utilizar comillas
+var propiedad = "nombre";
+var propiedad2 = "edad";
+var propiedad3 = "genero";
+
+console.log(myObject[propiedad]);
+console.log(myObject[propiedad2]);
+console.log(myObject[propiedad3]);
+   
+   
+   
+
+
+ 
+
+
+  
+          
+
+
+
+   
+
+
