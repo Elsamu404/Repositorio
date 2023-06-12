ff93a03a5158e5bdf4b3308d5f0fff166fd3c16f
Eliel Samuel Garcia Martinez <127472305+Elsamu404@users.noreply.github.com>
2023-06-11 19:03:49 -0600

    Add files via upload

---
+++ Ciclos.js
@@ -0,0 +1,32 @@
+function generarSecuencia() {
+  let numero = 1;
+  let incremento = 1;
+
+  for (let i = 0; i <= 10; i++) {
+    console.log(numero);
+    numero += incremento;
+    incremento++;
+  }
+}
+      
+      
+      // Ejemplo de uso
+      const secuenciaGenerada = generarSecuencia();
+      console.log(secuenciaGenerada);
+      
+   
+   
+   
+
+
+ 
+
+
+  
+          
+
+
+
+   
+
+
